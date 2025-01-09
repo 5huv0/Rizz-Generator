f636ca016c5b424b079a8786f192f6683da31fc1
5huv0 <md88748874@gmail.com>
2025-01-10 02:05:26 +0600

    contact page

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Contacts() {
   return (
-    <div>
-      <p>
-        Contacts
-      </p>
+    <div className='flex flex-col'>
+
+      <div className='bg-green-500 w-9/12 p-10'>
+        <p className='font-extrabold text-7xl flex'>
+          Connect with me Here
+        </p>
+      </div>
+
+      <div className='bg-green-500 w-9/12 p-10 '>
+        <button>
+            <Link
+              to="https://github.com/5huv0"
+            >
+              <img src="/images/github-logo.png" alt="" />
+            </Link>
+          </button>
+          <button>
+            <Link
+              to=""
+            >
+              <img src="/images/fb-logo.png" alt="" />
+            </Link>
+          </button>
+          <button>
+            <Link
+              to=""
+            >
+              <img src="/images/insta-logo.png" alt="" />
+            </Link>
+          </button>
+          <button>
+            <Link
+              to=""
+            >
+              <img src="/images/twitter-logo.png" alt="" />
+            </Link>
+          </button>
+          <button>
+            <Link
+              to=""
+            >
+              <img src="/images/gmail-logo.png" alt="" />
+            </Link>
+          </button>
+      </div>
     </div>
   )
 }
