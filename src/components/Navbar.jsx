07633abbd388cc583a06551a83406f04637bbf50
5huv0 <md88748874@gmail.com>
2025-01-09 13:43:58 +0600

    navbar complete and next planning done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,42 +2,44 @@ import React, { useState } from 'react'
 
 export default function Navbar() {
 
+  // const [color, setColor] = useState("#FFFFFF")
+
   const [isImageOne, setIsImageOne] = useState(true);
 
   const changeImage = () => {
     setIsImageOne(!isImageOne); // Toggle the state
+    // setColor("#000000")
   };
 
   return (
-      <nav className='flex bg-blue-300 p-3 justify-between'>
-
-        <div className='flex '> {/* This is logo and name section */}
-          <img src="./images/logo-3.png" alt="" className='h-10'/>
-          <img src="./images/name-2.png" alt="" className='h-10' />
-        </div>
-
-        <div className='flex space-x-4'> {/* This is buttons section */}
-          <button className='bg-blue-900 text-white p-3 rounded-xl hover:bg-blue-600'>
-            Favourites
-          </button>
-          <button className='bg-blue-900 text-white p-3 rounded-xl hover:bg-blue-600'>
-            Add New Rizz
-          </button>
-          <button className='bg-blue-900 text-white p-3 rounded-xl hover:bg-blue-600'>
-            Connect with me 
-          </button>
-         
+    //  <div style={{backgroundColor : color}} >
+      <nav className='flex bg-gray-300 p-3 justify-between drop-shadow-lg'>
+          <div className='flex '> {/* This is logo and name section */}
+            <img src="./images/logo-3.png" alt="" className='h-10'/>
+            <img src="./images/name-2.png" alt="" className='h-10' />
+          </div>
 
 
-          <img
-            src={isImageOne ? "./images/lite-mode.png" : "./images/dark-mode.png"}
-            onClick={changeImage}
-            className='hover:cursor-pointer h-10'
-          />
-        
-          
-        </div>
-
+          <div className='flex space-x-4 font-bold font-mono'> {/* This is buttons section */}
+            <button className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'>
+              Favourites
+            </button>
+            <button className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'>
+              Add Your Rizz
+            </button>
+            <button className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'>
+               Features
+            </button>
+            <button className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'>
+              Contacts
+            </button>
+            <img
+              src={isImageOne ? "./images/lite-mode.png" : "./images/dark-mode.png"}
+              onClick={changeImage}
+              className='hover:cursor-pointer h-10'
+            />
+          </div>
       </nav>
+    // </div>
   )
 }
