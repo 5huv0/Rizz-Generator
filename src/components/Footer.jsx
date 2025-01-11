b97a68719435ed15c3b19b2b179138deae421d3c
5huv0 <md88748874@gmail.com>
2025-01-12 01:40:04 +0600

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,45 +3,85 @@ import { Link, NavLink } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div className='bg-slate-500 w-full h-32 fixed bottom-0 flex justify-around'>
-      <div>
+    <div className='bg-slate-500 w-full fixed bottom-0 left-0 right-0 flex justify-around'>
+      <div className='flex flex-col items-start'>
         <p className='font-bold font-mono text-black text-4xl p-3'>
             Be Cheezy
         </p>
+        <div className='flex '> {/* This is linkings part*/}
+                <button className='hover:bg-green-200 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+                    <Link
+                      to="https://github.com/5huv0"
+                    >
+                      <img src="/images/github-logo.png" alt="" />
+                    </Link>
+                </button>
+                <button className='hover:bg-green-200 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+                    <Link
+                      to="https://www.facebook.com/share/1DSDLmcNCB/"
+                    >
+                      <img src="/images/fb-logo.png" alt="" />
+                    </Link>
+                </button>
+                <button className='hover:bg-green-200 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+                    <Link
+                      to="https://www.instagram.com/5huv_0?igsh=ZDVuMm0wOTBtMnln"
+                    >
+                      <img src="/images/insta-logo.png" alt="" />
+                    </Link>
+                </button>
+                <button className='hover:bg-green-200 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+                    <Link
+                      to="https://x.com/MdShuvo76030551?t=EYDUmqMZbLgsENpAbjW4Kw&s=09"
+                    >
+                      <img src="/images/twitter-logo.png" alt="" />
+                    </Link>
+                </button>
+                <button className='hover:bg-green-200 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+                    <Link
+                      to="mailto: md88748874@gmail.com"
+                    >
+                      <img src="/images/gmail-logo.png" alt="" />
+                    </Link>
+                </button>
+        </div>
+        <p className='font-thin font-mono text-black text-xs p-3'>
+            All Right Reserve @5huv0
+        </p>
       </div>
       <div>
-        <div className='flex  space-x-4 font-bold font-mono'> {/* This is buttons section */}
+        <div className='flex flex-col font-bold font-mono p-3 m-2'> {/* This is buttons section */}
             <NavLink
                 to="/rizzgen"
-                className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'
+                className=' text-black  rounded-xl hover:underline'
                 >
                 Create Rizz
                 </NavLink>
                 <NavLink
                 to="/favourites"
-                className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'
+                className=' text-black  rounded-xl hover:underline'
                 >
                 Favourites
                 </NavLink>
                 <NavLink
                 to="/addrizz"
-                className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'
+                className=' text-black  rounded-xl hover:underline'
                 >
                 Add Your Rizz
                 </NavLink>
                 <NavLink
                 to="/features"
-                className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'
+                className=' text-black  rounded-xl hover:underline'
                 >
                 Features
                 </NavLink>
                 <NavLink
                 to="/contacts"
-                className=' text-black p-3 rounded-xl hover:text-blue-600 hover:underline'
+                className=' text-black rounded-xl hover:underline'
                 >
                 Contacts
-                </NavLink>
-            </div>
+            </NavLink>
+        </div>
       </div>
     </div>
   )
