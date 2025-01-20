d7f40cfa05966b7104d031cdc2442b29c6972d61
5huv0 <md88748874@gmail.com>
2025-01-21 01:53:52 +0600

    working with features page

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,90 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Features() {
   return (
     <div>
-      <p>
-        
-      </p>
+      <div> {/* div section 1 */}
+        <p>Rizz Generator & Features</p>
+        <p>Everything you need to flirt</p>
+        <Link to="#" className=' text-black rounded-xl hover:!text-blue-500'>
+          Try Now
+        </Link>
+        <Link to="/contacts" className=' text-black rounded-xl  hover:!text-blue-500'>
+          Contact Us
+        </Link>
+      </div> 
+      <div> {/* div section 2 */}
+        <p>
+          Features
+        </p>
+        <div className='flex space-x-4'>
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card text-center" >
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div> 
     </div>
   )
 }
