4262fa9f6e4e20a5327dba04e93b5be52139a7ac
5huv0 <md88748874@gmail.com>
2025-01-22 01:02:30 +0600

    almost done with features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,26 +4,38 @@ import { Link } from 'react-router-dom'
 export default function Features() {
   return (
     <div>
-      <div> {/* div section 1 */}
-        <p>Rizz Generator & Features</p>
-        <p>Everything you need to flirt</p>
-        <Link to="#" className=' text-black rounded-xl hover:!text-blue-500'>
-          Try Now
-        </Link>
-        <Link to="/contacts" className=' text-black rounded-xl  hover:!text-blue-500'>
-          Contact Us
-        </Link>
+
+      <div className='bg-blue-500 flex justify-center items-center flex-col p-24  gap-10'> {/* div section 1 */}
+        <p className='font-mono text-5xl text-white'>
+          Rizz Generator & Features
+        </p>
+        <p className='font-mono text-3xl text-white'>
+          Everything you need to flirt
+        </p>
+        <div className='button-div flex gap-4'>
+          <Link to="#" >
+            <button className=' text-black rounded-xl bg-white hover:!text-red-600 p-3 m-2'>
+              Try Now
+            </button>
+          </Link>
+          <Link to="/contacts" >
+            <button className=' text-black rounded-xl bg-white hover:!text-red-600 p-3 m-2'>
+              Contact Us
+            </button>
+          </Link>
+        </div>
       </div> 
+
       <div> {/* div section 2 */}
-        <p>
+        <p className='font-mono font-extrabold text-5xl flex justify-center align-middle'>
           Features
         </p>
-        <div className='flex space-x-4'>
+        <div className='grid grid-cols-4 gap-4 m-3'>
           <div className="card text-center" >
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500 rounded-xl ">Go somewhere</a>
             </div>
           </div>
 
@@ -31,7 +43,7 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
           </div>
           
@@ -40,7 +52,7 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
           </div>
 
@@ -48,7 +60,7 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
           </div>
 
@@ -56,7 +68,7 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
           </div>
 
@@ -64,7 +76,7 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
           </div>
 
@@ -72,7 +84,7 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
           </div>
 
@@ -80,11 +92,12 @@ export default function Features() {
             <div className="card-body">
               <h5 className="card-title">Special title treatment</h5>
               <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="bg-green-500  rounded-xl ">Go somewhere</a>
             </div>
-          </div>]
+          </div>
         </div>
       </div> 
+
     </div>
   )
 }
