9f18f11e8752ab07ea47672419dd1ab7d703511c
5huv0 <md88748874@gmail.com>
2025-01-22 21:40:13 +0600

    working with main page

--- src/components/RizzGen.jsx
+++ src/components/RizzGen.jsx
@@ -11,19 +11,19 @@ export default function RizzGen() {
         </p>
       </div>
       
-      <div  className="dropdown flex justify-center items-center p-5">
-        <button  className="btn btn-secondary dropdown-toggle p-6" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <div  className="dropdown flex justify-center items-center p-5 m-5">
+        <button  className="btn btn-secondary dropdown-toggle p-6 font-mono" type="button" data-bs-toggle="dropdown" aria-expanded="false">
           Choose Your Language
         </button>
-        <ul  className="dropdown-menu ">
-          <li><a  className="dropdown-item" href="#">Bangla</a></li>
-          <li><a  className="dropdown-item" href="#">Urdu</a></li>
-          <li><a  className="dropdown-item" href="#">English</a></li>
-          <li><a  className="dropdown-item" href="#">Spanish</a></li>
+        <ul  className="dropdown-menu">
+          <li><a  className="dropdown-item font-mono" href="#">Bangla</a></li>
+          <li><a  className="dropdown-item font-mono" href="#">Urdu</a></li>
+          <li><a  className="dropdown-item font-mono" href="#">English</a></li>
+          <li><a  className="dropdown-item font-mono" href="#">Spanish</a></li>
         </ul>
       </div>
 
-      <div className='text-xl font-serif flex justify-center items-center flex-col p-4'>
+      <div className='text-xl font-serif flex justify-center items-center flex-col p-4 '>
         <p className='text-2xl font-bold font-mono text-white border-4 p-5'>
           Aren't you worried about global warming? Because you're making it hot in here.
         </p>
