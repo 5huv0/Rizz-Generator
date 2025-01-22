52a68b2e249d78ad9c57e6188454cd7f1a441f5d
5huv0 <md88748874@gmail.com>
2025-01-23 01:06:34 +0600

    done with the help of chat gpt

--- src/components/RizzGen.jsx
+++ src/components/RizzGen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import banglaRizz from './RizzDataBan';
 import englishRizz from './RizzDataEng';
 import hindiRizz from './RizzDataHin';
@@ -8,6 +8,26 @@ import spanishRizz from './RizzDataSpn';
 
 
 export default function RizzGen() {
+  const[selectedLang , setSelectedLang] = useState('english')
+  const[currentRizz , setCurrentRizz] = useState('')
+
+  const rizzData = {
+    english : englishRizz,
+    bangla : banglaRizz,
+    hindi : hindiRizz,
+    spanish : spanishRizz
+  }
+
+  const generateRandomRizz = () => {
+    const selectRizzData = rizzData[selectedLang];
+    const randNum = Math.floor(Math.random() * selectRizzData.length);
+    setCurrentRizz(selectRizzData[randNum].rizz);
+  }
+
+  const langChng = (language) => {
+    setSelectedLang(language);
+    generateRandomRizz();
+  }
   return (
     <div className='bg-blue-500'>
 
@@ -22,19 +42,19 @@ export default function RizzGen() {
           Choose Your Language
         </button>
         <ul  className="dropdown-menu">
-          <li><a  className="dropdown-item font-mono" href="#">Bangla</a></li>
-          <li><a  className="dropdown-item font-mono" href="#">Urdu</a></li>
-          <li><a  className="dropdown-item font-mono" href="#">English</a></li>
-          <li><a  className="dropdown-item font-mono" href="#">Spanish</a></li>
+          <li><a  className="dropdown-item font-mono" href="#" onClick={() => langChng('bangla')}>Bangla</a></li>
+          <li><a  className="dropdown-item font-mono" href="#" onClick={() => langChng('hindi')}>Hindi</a></li>
+          <li><a  className="dropdown-item font-mono" href="#" onClick={() => langChng('english')}>English</a></li>
+          <li><a  className="dropdown-item font-mono" href="#" onClick={() => langChng('spanish')}>Spanish</a></li>
         </ul>
       </div>
 
       <div className='text-xl font-serif flex justify-center items-center flex-col p-4 '>
         <p className='text-2xl font-bold font-mono text-white border-4 p-5'>
-          Aren't you worried about global warming? Because you're making it hot in here.
+          {currentRizz || "Click Generate one to Generate a Rizz Line!!!"}
         </p>
-        <button className='bg-emerald-300 font-bold p-3 rounded-xl flex justify-center items-center m-5'>
-          Another One <img src="./images/loop-logo.png" alt="" className='size-5'/>
+        <button className='bg-emerald-300 font-bold p-3 rounded-xl flex justify-center items-center m-5' onClick={generateRandomRizz} >
+          Generate One <img src="./images/loop-logo.png" alt="" className='size-5'/>
         </button>
       </div>
     </div>
